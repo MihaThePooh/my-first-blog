e83e6c835780740362c0d448c111b3454aa6014e
MihaThePooh <mihailpero90@gmail.com>
2020-04-20 09:43:42 +0300

    Change static file directory on /blog/.

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 #STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_URL = '/blog/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static'), ]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "blog/static")
